6135eb5be19e32a72d5f41ad31b492df2586ad4f
KhaledToha <96600177+KhaledToha@users.noreply.github.com>
2023-04-11 14:10:01 +0200

    Update addUser.js

--- server/database/queries/addUser.js
+++ server/database/queries/addUser.js
@@ -7,13 +7,13 @@ require('dotenv').config();
 const key = process.env.SECRET_KEY
 
 const addUser = (userData) => {
-  const { name, avatarInput, emailInput, password } = userData;
+  const { name, avatarInput, emailInput, password, role, mobile } = userData;
   const hashedPassword = hashingFun(password,key);
   
   return pool.query(
     
-    "INSERT INTO users (name, img_url, email, password, role) VALUES ($1, $2, $3, $4, $5) RETURNING *",
-    [name, avatarInput, emailInput, hashedPassword]
+    "INSERT INTO users (name, img_url, email, password, role, mobile) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *",
+    [name, avatarInput, emailInput, hashedPassword, role, mobile]
   );
 };
 
