503e0da0d1199a293639b0cb225f55f77769b041
KhaledToha <khaled.f.toha@gmail.com>
2023-04-12 01:20:35 +0200

    finishing user page rendering relates #19 closes #19

--- server/controller/hashing.js
+++ server/controller/hashing.js
@@ -2,7 +2,7 @@
 const jwt = require('jsonwebtoken');
 const hashingFun = (notHashed,key) => {
     return new Promise((resolve,reject)=>{
-
+        let result ;
         jwt.sign(notHashed,key,(err,hashed)=>{
            if (err) {
                reject(err)
